d02ea291c1f7b2da0a5e619af62bb00b5649f43e
Blizzarre <Blizzarre@users.noreply.github.com>
2019-09-11 20:38:36 +0700

    Implemented scroll wheel control in main menu

--- Scenes/MainButtonsController.gd
+++ Scenes/MainButtonsController.gd
@@ -31,23 +31,52 @@ func _ready():
 
 var rotating = false
 
+var scroll_rotate_count:int = 0
+var time_since_last_scroll:float = 0
+const SCROLL_LIMIT = 0.08
+
+func _input(event):
+    # Wheel Up Event
+	if event.is_action_pressed("ui_scroll_down") or event.is_action_pressed("ui_scroll_up"):
+		if event.is_action_pressed("ui_scroll_down"):
+			if(scroll_rotate_count> -1):
+				scroll_rotate_count-=1
+	    # Wheel Down Event
+		elif event.is_action_pressed("ui_scroll_up"):
+			if(scroll_rotate_count< 1):
+				scroll_rotate_count+=1
+
+		if scroll_rotate_count != 0:
+			if rotating == false:
+				time_since_last_scroll = SCROLL_LIMIT if time_since_last_scroll < SCROLL_LIMIT else time_since_last_scroll
+				rotate_speed(sign(scroll_rotate_count),1.3+0.2/time_since_last_scroll)
+				scroll_rotate_count-=sign(scroll_rotate_count)
+		time_since_last_scroll = 0
+
+
+
 func _process(delta):
-	
+	if(time_since_last_scroll<1):
+		time_since_last_scroll+=delta
+		
 	if !Input.is_action_pressed("ui_down") or !Input.is_action_pressed("ui_up"):
 		if Input.is_action_pressed("ui_down"):
 			if rotating == false:
-				rotate(-1)
+				rotate_speed(-1,1.5)
 	
 		if Input.is_action_pressed("ui_up"):
 			if rotating == false:
-				rotate(1)
+				rotate_speed(1,1.5)
+
 
 var direction = 0
 const ANIM_LENGTH = 0.25
 onready var tween = $MainButtonTween
 
-
 func rotate(dir:int):
+	rotate_speed(dir,1)
+
+func rotate_speed(dir:int, speed_scale:float):
 	rotating = true
 	direction = dir
 	if dir == 1:
@@ -55,18 +84,20 @@ func rotate(dir:int):
 		button_instances.push_front(tmp)
 		
 		tween.interpolate_method(self, "rotate_to", 0, 
-		rad_increment, ANIM_LENGTH, Tween.TRANS_BACK, Tween.EASE_OUT,0)
+		rad_increment, ANIM_LENGTH/speed_scale, Tween.TRANS_BACK, Tween.EASE_OUT,0)
 		
 		tween.interpolate_property(cog, "rotation_degrees", cog.rotation_degrees, 
-		cog.rotation_degrees+rad2deg(rad_increment), ANIM_LENGTH, 
+		cog.rotation_degrees+rad2deg(rad_increment), ANIM_LENGTH/speed_scale, 
 		Tween.TRANS_BACK, Tween.EASE_OUT,0)
 	elif dir == -1:
 		tween.interpolate_method(self, "rotate_to", 0, 
-		-rad_increment, ANIM_LENGTH, Tween.TRANS_BACK, Tween.EASE_OUT,0)
+		-rad_increment, ANIM_LENGTH/speed_scale, Tween.TRANS_BACK, Tween.EASE_OUT,0)
 		
 		tween.interpolate_property(cog, "rotation_degrees", cog.rotation_degrees, 
-		cog.rotation_degrees+rad2deg(-rad_increment), ANIM_LENGTH, 
+		cog.rotation_degrees+rad2deg(-rad_increment), ANIM_LENGTH/speed_scale, 
 		Tween.TRANS_BACK, Tween.EASE_OUT,0)
+	else:
+		return
 	tween.start()
 
 func rotate_to(rad):
@@ -79,10 +110,9 @@ func rotate_to(rad):
 		#btn.set_rotation(rad2deg(target_radius.angle()))
 		target_radius = target_radius.rotated(rad_increment)
 
-func _on_MainButtonTween_tween_completed(object, key):
+func _on_MainButtonTween_tween_all_completed():
 	rotating = false
 	#print("finished tween")
-	if object == self:
-		if direction == -1:
-			var tmp = button_instances.pop_front()
-			button_instances.push_back(tmp)
+	if direction == -1:
+		var tmp = button_instances.pop_front()
+		button_instances.push_back(tmp)
