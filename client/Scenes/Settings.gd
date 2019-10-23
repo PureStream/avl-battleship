2bcf9c88887649d949939c93b0843e33e60c05a8
IIpolar <baipo1999@gmail.com>
2019-10-23 20:59:31 +0700

    login page username fixed

--- client/Scenes/Settings.gd
+++ client/Scenes/Settings.gd
@@ -3,7 +3,7 @@ extends Node
 onready var settings_file = "user://settings.save"
 var login_email := ""
 var login_password := ""
-var password := ""
+var is_remember_password = false
 var sound_value = 50
 var music_value = 50
 
@@ -13,20 +13,21 @@ func _ready():
 func save():
 	var save_dict = {
 		"email": login_email,
-		"password": password,
+		"password": login_password,
 		"sound_value": sound_value,
-		"music_value": music_value
+		"music_value": music_value,
+		"is_remember_password": is_remember_password
 	}
 	return save_dict
 	
-func save_email():
+func save_profile():
 	var f := File.new()
 	f.open(settings_file, File.WRITE)
 	var to_save = save()
 	f.store_line(to_json(to_save))
 	f.close()
 
-func load_email():
+func load_profile():
 	var f := File.new()
 	if not f.file_exists("user://settings.save"):
 		return
@@ -34,4 +35,5 @@ func load_email():
 	var curr_line = parse_json(f.get_line())
 	login_email = curr_line["email"]
 	login_password = curr_line["password"]
+	is_remember_password = curr_line["is_remember_password"]
 	f.close()
