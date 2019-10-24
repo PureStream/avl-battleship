b0a5a0e9c0a003e02862ec3dd9a066604c8d7c2e
IIpolar <baipo1999@gmail.com>
2019-10-24 12:53:02 +0700

    music persist through scenes slider can really use

--- client/Scenes/Screens/Options.gd
+++ client/Scenes/Screens/Options.gd
@@ -6,11 +6,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	Settings.load_profile()
 
 
 func _on_CheckButton_toggled(button_pressed):
@@ -19,7 +15,14 @@ func _on_CheckButton_toggled(button_pressed):
 
 func _on_SoundContainer_update_value(value):
 	Settings.sound_value = value
+	#AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound"), value-45)
+	Settings.save_profile()
 
 
 func _on_MusicContainer_update_value(value):
 	Settings.music_value = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value-45)
+	Settings.save_profile()
+
+func _on_LogOutButton_pressed():
+	get_tree().change_scene("res://Scenes/Screens/LoginPage.tscn")
--- client/Sounds/ClickSound.gd
+++ client/Sounds/ClickSound.gd
@@ -6,7 +6,8 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _process(delta):
+	pass
 	position = get_global_mouse_position()
 	if Input.is_action_just_pressed("Left_Mouse"):
 		click.play()
-
+	
